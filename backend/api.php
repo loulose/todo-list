4a03bf49850b51f8346085f22d124b8fae98fc31
Kyle Pierce <uplexa@protonmail.com>
2020-03-11 16:08:51 +0700

    [Front->Back] API response, list tasks from DB, new task, delete, move position, mark complete, failsafes

--- backend/api.php
+++ backend/api.php
@@ -1,5 +1,6 @@
 <?php
 require_once("config.php");
+header("Content-Type: application/json; charset=UTF-8");
 
 if($_POST){ // Secure & map all input requests by default.
   $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -9,67 +10,138 @@ if($_POST){ // Secure & map all input requests by default.
 
 function db($query){ // DB Query function
   $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
   if ($db->connect_error) { // Record any DB connection errors.
     return "dbError: " . $db->error;
   }
-
   $result = $db->query($query); // Execute query, store as var
-
   if(!$result){
     return "dbError: " . $db->error;
   } else{ // Success
     return $result;
-}
-
+  }
   $db->close();
 }
-
 
 function newTask($taskName){
-  $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  $orderID = db("SELECT id FROM tasks ORDER BY id DESC LIMIT 1");
-  $orderID = $orderID->fetch_assoc()["id"] + 1;
-  $db->close();
+  if(strlen($taskName)>0){ // Make sure task name is not empty
+    $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $orderID = db("SELECT id FROM tasks ORDER BY id DESC LIMIT 1");
+    $orderID = $orderID->fetch_assoc()["id"] + 1;
+    $db->close();
 
-  $returnData = db("INSERT INTO tasks (taskName, taskOrder) VALUES ('$taskName', '$orderID')");
-  if(strpos($returnData, "dbError") !== false){ // Success
-    $error = explode("dbError: ", $returnData); // Failure
-    $clientResponse = array("error"=>"true", "task"=>"new", "description:"=>"database error", "errorDump"=>$error[1]);
-    http_response_code(400);
-    printf(json_encode($clientResponse));
-  }else{
-    $clientResponse = array("error"=>"false", "task"=>"new", "description:"=>"");
+    $returnData = db("INSERT INTO tasks (taskName, taskOrder) VALUES ('$taskName', '$orderID')");
+    if(strpos($returnData, "dbError") !== false){ // Success
+      $error = explode("dbError: ", $returnData); // Failure
+      $clientResponse = array(
+        "error"=>"true", "task"=>"new", "description:"=>"database error", "errorDump"=>$error[1]
+      );
+      http_response_code(400);
+      printf(json_encode($clientResponse));
+    }else{
+      $clientResponse = array(
+        "error"=>"false", "task"=>"new", "description:"=>"", "id"=>$orderID, "name"=>$taskName
+      );
+      http_response_code(200);
+      printf(json_encode($clientResponse));
+    }
+    return $orderID;
+  } else{ // Task name is empty
+    $clientResponse = array("error"=>"true", "task"=>"new", "description:"=>"Task name is empty");
     http_response_code(200);
     printf(json_encode($clientResponse));
   }
 }
 
 function deleteTask($id){
-
+  $returnData = db("DELETE FROM tasks WHERE taskOrder='$id'");
+  if(strpos($returnData, "dbError") !== false){ // Success
+    $error = explode("dbError: ", $returnData); // Failure
+    $clientResponse = array(
+      "error"=>"true", "task"=>"new", "description:"=>"database error", "errorDump"=>$error[1]
+    );
+    http_response_code(400);
+    printf(json_encode($clientResponse));
+  }else{
+    $clientResponse = array(
+      "error"=>"false", "task"=>"deleted", "description:"=>"", "id (order position)"=>$id
+    );
+    http_response_code(200);
+    printf(json_encode($clientResponse));
+  }
 }
 
-function updateTask($id, $complete, $method){
+function updateTask($id, $targetID = 0, $method, $complete){
   /*
   Methods:
   1 = Update ID/Re-order, 2 = mark complete/incomplete
   */
+  $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
   if($method=="1"){
+    /*
+    UPDATE tasks SET
 
+    taskOrder = CASE
+    WHEN `taskOrder`='2' THEN '1'
+    WHEN `id`='1' THEN '2'
+    ELSE `taskOrder` END
+    */
+    console.log("ID: " + $id + " Target ID: " + $targetID);
+    $returnData = db("UPDATE tasks SET taskOrder = CASE
+      WHEN taskOrder = '$targetID' THEN '$id'
+      WHEN taskOrder = '$id' THEN '$targetID'
+      ELSE taskOrder END");
+    if(strpos($returnData, "dbError") !== false){ // Success
+      $error = explode("dbError: ", $returnData); // Failure
+      $clientResponse = array(
+        "error"=>"true", "task"=>"new", "description:"=>"database error", "errorDump"=>$error[1]
+      );
+      http_response_code(400);
+      printf(json_encode($clientResponse));
+    }else{
+      $clientResponse = array(
+        "error"=>"false", "task"=>"updated", "description:"=>"", "id"=>$id, "targetID"=>$targetID
+      );
+      http_response_code(200);
+      printf(json_encode($clientResponse));
+    }
   }
-  if($method=="2"){ // 1 = Update ID/Re-order, 2 = mark complete/incomplete
-
+  if($method=="2"){
+    $complete = (int)($complete === 'true'); // Convert our boolean to an interger for DB
+    $returnData = db("UPDATE tasks SET status = '$complete' WHERE taskOrder='$id'");
+    if(strpos($returnData, "dbError") !== false){ // Success
+      $error = explode("dbError: ", $returnData); // Failure
+      $clientResponse = array(
+        "error"=>"true", "task"=>"new", "description:"=>"database error", "errorDump"=>$error[1]
+      );
+      http_response_code(400);
+      printf(json_encode($clientResponse));
+    }else{
+      $clientResponse = array(
+        "error"=>"false", "task"=>"updated", "description:"=>"", "status"=>$complete
+      );
+      http_response_code(200);
+      printf(json_encode($clientResponse));
+    }
   }
+  $db->close();
 }
 
 function pullTasks(){ // Pull tasks from DB
-  $taskArray = array();
+  $itemsArray = array();
+  $itemsArray["records"] = array();
   $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  $tasks = db("SELECT taskOrder, taskName, status FROM tasks ORDER BY taskOrder DESC");
+  $tasks = db("SELECT id, taskOrder, taskName, status FROM tasks ORDER BY taskOrder ASC");
   while($row = $tasks->fetch_assoc()) {
-    $taskArray[] = $row;
+    $taskItem = array(
+      "id" => $row["id"],
+      "taskOrder" => $row["taskOrder"],
+      "taskName" => $row["taskName"],
+      "status" => $row["status"]
+    );
+    array_push($itemsArray["records"], $taskItem);
   }
-  printf(json_encode($taskArray));
+  http_response_code(200);
+  printf(json_encode($itemsArray));
   $db->close();
 }
 
@@ -77,9 +149,9 @@ function pullTasks(){ // Pull tasks from DB
 if($_POST["newTask"]=="exec"){
   newTask($data["taskTitle"]);
 }else if($_POST["deleteTask"]=="exec"){
-  deleteTask(data["id"]);
+  deleteTask($data["id"]);
 }else if($_POST["updateTask"]=="exec"){
-  updateTask(data["id"], data["complete"], data["method"]);
+  updateTask($data["id"], $data["targetID"], $data["method"], $data["complete"]);
 }
 else if($_POST["pullTasks"]=="all"){
   pullTasks();
@@ -88,7 +160,7 @@ else if($_POST["pullTasks"]=="all"){
 /* No queries = error */
 else{
   $clientResponse = array("error"=>"true", "description:"=>"no input data");
-  http_response_code(404);
+  http_response_code(400);
   printf(json_encode($clientResponse));
 }
 
--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,41 +1,16 @@
-let taskList = document.getElementsByClassName("task");
-for (let i = 0; i < taskList.length; i++) {
-
-  // Create a checkbox and prepend it to each list item
-  let checkBox = document.createElement("DIV");
-  checkBox.setAttribute('id', 'customCheckbox');
-  checkBox.setAttribute('class', 'customCheckbox');
-  taskList[i].insertBefore(checkBox, taskList[i].firstChild);
-
-  // Create a checkbox and prepend it to each list item
-  let trash = document.createElement("IMG");
-  trash.setAttribute('src', './assets/media/trash.png');
-  trash.setAttribute('height', '40px');
-  trash.className = "delete";
-  taskList[i].appendChild(trash);
-
-  // Create a vertical line and append it to each list item
-  let span = document.createElement("SPAN");
-  span.setAttribute('class', 'vertSplit');
-  taskList[i].appendChild(span);
-
-  // Create a second vertical line and append it to each list item
-  let span2 = document.createElement("SPAN");
-  span2.setAttribute('class', 'vertSplit2');
-  taskList[i].appendChild(span2);
-}
-
 // Click on delete button to hide the current list item
 let del = document.getElementsByClassName("delete");
 for (let i = 0; i < del.length; i++) {
   del[i].onclick = function() {
     let div = this.parentElement;
     div.style.display = "none";
+    let id = div.id;
+    console.log(id);
   }
 }
 
-// Create a new list item
-function newElement() {
+// Create a new list item based off of values pulled from backend
+function newElement(id, taskName, taskStatus) {
   let li = document.createElement("li");
 
   let span = document.createElement("span");
@@ -47,22 +22,29 @@ function newElement() {
   let span3 = document.createElement("span");
   span3.className = "taskText";
 
-  li.className = "task";
+  if(taskStatus==1){ // If task is marked complete, set checked task
+    li.className = "checked task";
+  }else{ // Not complete, just set as normal task
+    li.className = "task";
+  }
+  // Set task ID, as we'll need this for re-arranging order via drag&drop + deleting from DB
+  li.id = id;
+  li.draggable = "true"; // Make the items dragable.
 
-  let inputValue = document.getElementById("taskTitle").value;
-  let dataSend = "newTask=exec&taskTitle=" + inputValue;
-  apiReq(dataSend);
+  let inputValue = taskName; //document.getElementById("taskTitle").value
+
   let t = document.createTextNode(inputValue);
   li.appendChild(span);
   li.appendChild(span2);
   li.appendChild(span3);
   span3.appendChild(t);
-
+/*
   if (inputValue === '') {
     alert("You must enter text to create a task!");
   } else {
     document.getElementById("taskUL").appendChild(li);
-  }
+  }*/
+  document.getElementById("taskUL").appendChild(li);
   document.getElementById("taskTitle").value = "";
 
   let checkBox = document.createElement("DIV");
@@ -80,15 +62,48 @@ function newElement() {
     del[i].onclick = function() {
       let div = this.parentElement;
       div.style.display = "none";
+      let id = div.id;
+      console.log(id);
+      deleteTask(id);
     }
   }
 
 }
 
+function newTask(inputText){
+  let dataSend = "newTask=exec&taskTitle=" + inputText;
+  return apiReq(dataSend);
+}
+
+function deleteTask(id){
+  let dataSend = "deleteTask=exec&id=" + id;
+  return apiReq(dataSend, 3);
+}
+
+function moveTask(id, targetID){
+  let dataSend = "updateTask=exec&method=1&id=" + id + "&targetID=" + targetID;
+  return apiReq(dataSend, 3);
+  // Now we need to update the ID on the frontend in-case we move anything else around/delete/mark completed
+  let originalListID = document.getElementbyId(id);
+  let targetListID = document.getElementById(targetID);
+  originalListID.setAttribute("id", targetID);
+  targetListID.setAttribute("id", id);
+}
+
+function markTask(id, status){
+  let dataSend = "updateTask=exec&method=2&id=" + id + "&complete=" + status;
+  return apiReq(dataSend, 3);
+}
+
 // Creates new element (line item) when hitting enter
 document.getElementById('taskTitle').onkeydown = function(event) {
     if (event.keyCode == 13) {
-        newElement()
+      let inputText = document.getElementById("taskTitle").value;
+      if (inputText === '') {
+        alert("You must enter text to create a task!");
+      } else {
+        newTask(inputText);
+      }
     }
 }
 
@@ -99,6 +114,8 @@ list.addEventListener('click', function(ev) {
   if (cb.tagName === 'DIV') {
     cb = cb.parentNode;
     cb.classList.toggle('checked');
+    let checkStatus = cb.classList.contains('checked');
+    markTask(cb.id, checkStatus);
   }
 }, false);
 
@@ -132,7 +149,8 @@ let dragList;
           indexSwitch = i;
         }
       }
-      console.log(index, indexSwitch); // index/indexSwitch will be used for backend tomorrow
+      moveTask(id, target.id); // Update database to reflect order of tasks
+      //console.log(index, indexSwitch, target.id, id); // index/indexSwitch will be used for backend tomorrow
       if(index > indexSwitch) {
         target.before( dragged );
       } else {
@@ -141,17 +159,48 @@ let dragList;
     }
   });
 
+
+
   /***** BACKEND CONNECTIONS *****/
 let reqFile = "../backend/api.php";
-function apiReq(data) {
+function apiReq(data, type = 1) { // Type 1 = push, type 2 == pull (Actually, either way we need to repull)
   let request = new XMLHttpRequest();
-  request.onreadystatechange = function(){
-    if (this.status == 200) {
-      // Success
-    }
+  if(type=="2"){
+    request.onreadystatechange = function(){
+        if(this.readyState==4){
+        let list = JSON.parse(this.responseText);
+        console.log(list);
+        for (var record of list.records){
+             newElement(record.taskOrder, record.taskName, record.status);
+          }
+       }
+   }
+  }else if(type=="1"){
+    request.onreadystatechange = function(){
+      if(this.readyState==4){
+        let response = JSON.parse(this.responseText);
+        newElement(response.taskOrder, response.name, 0);
+        console.log(response);
+      }
+     }
+  } else{
+    request.onreadystatechange = function(){
+      if(this.readyState==4){
+        let response = JSON.parse(this.responseText);
+        console.log(response);
+      }
+     }
   }
   request.open("POST", reqFile, true);
   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   request.send(data);
-  console.log(request); // Debug purposes.
 }
+
+// Get Todo list from backend in JSON format, send to newElement() to popularize list.
+function getList(){
+  // Re-add list items with any new information
+  let dataSend = "pullTasks=all";
+  let data = apiReq(dataSend, 2);
+}
+
+getList();
